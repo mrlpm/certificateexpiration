b8e78af51b4a6032efc893e4cfb44645cae3ab97
Luis Pérez <luis.perez@protonmail.com>
2019-10-02 19:26:16 -0600

    first version

---
+++ certificateexpiration.py
@@ -0,0 +1,47 @@
+#!/bin/env python
+from __future__ import print_function
+import ssl
+import OpenSSL
+import datetime
+
+import smtplib
+from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
+
+def sendmail(content):
+    # Create the base text message.
+    msg = EmailMessage()
+    msg['Subject'] = "Remider of SSL"
+    msg['From'] = Address("Admin ALIGO", "admin@alimentosgourmet.com")
+    msg['To'] = (Address("Admin ALIGO", "admin@alimentosgourmet.com"),
+                 Address("Kevyn Perez", "kperez@itm.gt"))
+    msg.set_content(content)
+    with smtplib.SMTP('localhost') as s:
+        s.send_message(msg)
+
+
+def check():
+    hostname = "mail.alimentosgourmet.com"
+    port = 443
+
+    num_days = 7
+    cert = ssl.get_server_certificate(
+        (hostname, port), ssl_version=ssl.PROTOCOL_TLSv1)
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    expiry_date = x509.get_notAfter()
+    print(str(expiry_date))
+    assert expiry_date, "Cert doesn't have an expiry date."
+
+    ssl_date_fmt = r'%Y%m%d%H%M%SZ'
+    expires = datetime.datetime.strptime(str(expiry_date)[2:-1], ssl_date_fmt)
+    remaining = (expires - datetime.datetime.utcnow()).days
+
+    if remaining <= num_days:
+        body = "ALERTING!"
+        sendmail(body)
+    else:
+        print("Not alerting. You have " + str(remaining) + " days") 
+
+if '__main__' == __name__:
+    check()
